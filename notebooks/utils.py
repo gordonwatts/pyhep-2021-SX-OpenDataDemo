2d4f0867c8922bc9eee1694c4c9f4bf5c1c31b92
Gordon Watts <gordon@gordonandpaula.net>
2021-06-28 02:17:11 -0700

    First go at getting leptons from ATLAS

---
+++ notebooks/utils.py
@@ -0,0 +1,10 @@
+
+
+# The files we are going to be loading, along with necessary matching information
+files = {
+    'ggH125_ZZ4lep':
+        {
+            'files': ['root://eospublic.cern.ch//eos/opendata/atlas/OutreachDatasets/2020-01-22/4lep/MC/mc_345060.ggH125_ZZ4lep.4lep.root'],
+            'treename': 'ggH125_ZZ4lep'
+        },
+}
