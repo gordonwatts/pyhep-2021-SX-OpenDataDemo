56e644afe8d289d3d68ac18b115064d1cd1acf9c
Gordon Watts <gwatts@uw.edu>
2021-06-29 23:33:49 -0700

    We see data peak!!

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -2,9 +2,34 @@
 
 # The files we are going to be loading, along with necessary matching information
 files = {
+    'data':
+        {
+            'files': [
+                'root://eospublic.cern.ch//eos/opendata/atlas/OutreachDatasets/2020-01-22/4lep/Data/data_A.4lep.root',
+                'root://eospublic.cern.ch//eos/opendata/atlas/OutreachDatasets/2020-01-22/4lep/Data/data_B.4lep.root',
+                'root://eospublic.cern.ch//eos/opendata/atlas/OutreachDatasets/2020-01-22/4lep/Data/data_C.4lep.root',
+                'root://eospublic.cern.ch//eos/opendata/atlas/OutreachDatasets/2020-01-22/4lep/Data/data_D.4lep.root',
+                ],
+            'nickname': 'data'
+        },
     'ggH125_ZZ4lep':
         {
             'files': ['root://eospublic.cern.ch//eos/opendata/atlas/OutreachDatasets/2020-01-22/4lep/MC/mc_345060.ggH125_ZZ4lep.4lep.root'],
             'nickname': 'ggH125_ZZ4lep'
+        },
+    'ZH125_ZZ4lep':
+        {
+            'files': ['root://eospublic.cern.ch//eos/opendata/atlas/OutreachDatasets/2020-01-22/4lep/MC/mc_341947.ZH125_ZZ4lep.4lep.root'],
+            'nickname': 'ZH125_ZZ4lep'
+        },
+    'VBFH125_ZZ4lep':
+        {
+            'files': ['root://eospublic.cern.ch//eos/opendata/atlas/OutreachDatasets/2020-01-22/4lep/MC/mc_344235.VBFH125_ZZ4lep.4lep.root'],
+            'nickname': 'VBFH125_ZZ4lep'
+        },
+    'WH125_ZZ4lep':
+        {
+            'files': ['root://eospublic.cern.ch//eos/opendata/atlas/OutreachDatasets/2020-01-22/4lep/MC/mc_341964.WH125_ZZ4lep.4lep.root'],
+            'nickname': 'WH125_ZZ4lep'
         },
 }
